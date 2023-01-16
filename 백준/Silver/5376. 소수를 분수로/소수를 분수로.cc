9999c0e8128a5522cbeeeb4c33e73b7b11ff7631
YuJin Na <91943160+nayujin-dev@users.noreply.github.com>
2023-01-16 17:48:15 +0900

    [Silver I] Title: 소수를 분수로, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/5376. 소수를 분수로/소수를 분수로.cc
@@ -0,0 +1,97 @@
+#include<cstdio>
+
+int n;
+char arr[21];
+bool check[21];
+long long notinf, inf, bm, ibm, div,tot;
+int nonPt,rePt;
+long long gcd(long long a, long long b) {
+	long long c;
+	while (b) {
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+void trans() {
+	nonPt = 2;
+	rePt = 0;
+	//int i = 2;
+	notinf = inf = tot= 0;
+	bm = 10;
+	ibm = 0;
+	while (1) { //비순환구조 까지
+		if (arr[nonPt] == '(')
+		{
+			rePt = nonPt + 1; //rePt=순환소수의 첫번째자리 인덱스
+			nonPt--; //nonPt=비순환소수의 마지막자리 인덱스
+			break;
+		}
+		else if (arr[nonPt] == NULL) {
+			nonPt--;
+			break;
+		}
+		nonPt++;
+	}
+
+	//비순환소수 값 구하기
+	for (int j = 2; j <= nonPt; j++) { 
+		notinf += (arr[j] - '0');
+		inf += (arr[j]-'0');
+		//printf("현재 분자 %lld\n", notinf);
+		//if (j + 1 < nonPt) {
+		bm *= 10; //비순환소수 자리만큼 10곱함
+		notinf *= 10;
+		inf *= 10;
+
+		//}
+	}
+	
+	//순환소수 값 구하기
+	for (int j = rePt;; j++) {
+		if (arr[j] == ')'||j==0) {
+			ibm /= 10;
+			inf /= 10;
+			break;
+		}
+		inf += (arr[j]-'0');
+		inf *= 10;
+		ibm += 9;
+		ibm *= 10;
+	}
+
+	//순환구간 없을떄
+	if (ibm == 0) {
+		notinf /= 10;
+		bm /= 10;
+		div = gcd(bm,notinf);
+		//printf("순환구간 없고, 분자: %lld 분모: %lld\n",notinf,bm);
+		printf("%lld/%lld\n", notinf / div, bm / div);
+	}
+	else if (nonPt<=1) {//비순환 구간 없을때
+		div = gcd(ibm, inf);
+		//printf("비순환구간 없고, 분자: %lld 분모: %lld\n", inf, ibm);
+		printf("%lld/%lld\n", inf / div, ibm / div);
+	}
+	else { //둘다 있을때
+		notinf /= 10;
+		bm /= 10;
+		ibm *= bm;
+		//printf("섞여있고, inf: %lld notinf: %lld\n", inf, notinf);
+
+		inf -= notinf;
+		div = gcd(ibm, inf);
+		printf("%lld/%lld\n", inf / div, ibm / div);
+	}
+	return;
+}
+int main() {
+	
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%s", &arr);
+		trans();
+	}
+	return 0;
+}
