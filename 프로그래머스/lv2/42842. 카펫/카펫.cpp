e8e06357e8810ed44908c0ab124f9175bb2b658b
YuJin Na <91943160+nayujin-dev@users.noreply.github.com>
2023-06-23 22:28:07 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+// #include <iostream>
+using namespace std;
+
+vector<int> solution(int brown, int yellow) {
+    int garo,i=1;
+    garo=(brown-4)/2;
+    for(;i<=2501||i<=garo/2;i++){
+        if(i*(garo-i)==yellow){
+            break;
+        }
+    }
+    // cout<<"가로: "<<garo-i+2<<" 세로: "<<i+2<<endl;
+    
+    vector<int> answer;
+    
+    answer.push_back(garo-i+2);
+    answer.push_back(i+2);
+    return answer;
+}
