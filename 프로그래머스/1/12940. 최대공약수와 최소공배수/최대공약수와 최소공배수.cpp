562847b9ede4e1718f7126967bbcb5538e712885
Yujin Na <91943160+nayujin-dev@users.noreply.github.com>
2023-10-14 21:46:53 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -15,22 +15,13 @@ int gcd(int a, int b){
 }
 vector<int> solution(int n, int m) {
     vector<int> answer;
+    int num=gcd(n,m);
     
     // 최대공약수 구하기
-    answer.push_back(gcd(n,m));
-    // for(int i=min(n,m);i>=1;i--){
-    //     if(n%i==0 && m%i==0){
-    //         answer.push_back(i);
-    //         break;            
-    //     }
-    // }
+    answer.push_back(num);
     
     // 최소공배수 구하기
-    for(int i=min(n,m);i<=n*m;i++){
-        if(i%n==0 && i%m==0){
-            answer.push_back(i);
-            break;
-        }
-    }
+    answer.push_back(n*m/num);
+    
     return answer;
 }
