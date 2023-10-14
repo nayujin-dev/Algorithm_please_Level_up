7291a3ccf7e35eca4185fe11a171f07e1ffda594
Yujin Na <91943160+nayujin-dev@users.noreply.github.com>
2023-10-15 00:01:15 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, date_format(r.created_date, '%Y-%m-%d')
+from used_goods_board b, used_goods_reply r
+where b.board_id = r.board_id and  date_format(b.created_date,'%Y-%m')='2022-10' 
+order by r.created_date, b.title;
