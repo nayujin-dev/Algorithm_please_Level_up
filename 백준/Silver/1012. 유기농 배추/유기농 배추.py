a073e7793f241d6792714cf3284bcb354e3b6ee1
Yujin Na <91943160+nayujin-dev@users.noreply.github.com>
2023-10-16 22:42:40 +0900

    [Silver II] Title: 유기농 배추, Time: 292 ms, Memory: 31484 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -3,15 +3,17 @@ sys.setrecursionlimit(10000)
 
 
 def dfs(m, n):
-    if (n > N - 1) or (m > M - 1) or (m < 0) or (n < 0):
-        return
-    if visited[m][n] == 0 and cabbage[m][n] == 1:
-        visited[m][n] = 1
-        dfs(m + 1, n)
-        dfs(m, n + 1)
-        dfs(m-1, n)
-        dfs(m, n-1)
-    visited[m][n] = 1
+    cabbage[m][n] = 0
+    for j in range(4):
+        nx = m+dx[j]
+        ny = n+dy[j]
+        if(nx >= 0) and (ny >= 0) and (nx < M) and (ny < N):
+            if cabbage[nx][ny] == 1:
+                dfs(nx, ny)
+
+
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
 
 
 T = int(input())
@@ -19,17 +21,15 @@ for i in range(T):
     M, N, K = map(int, input().split())
 
     cabbage = [[0 for j in range(N)] for k in range(M)]
-    visited = [[0 for j in range(N)] for k in range(M)]
 
     for k in range(K):
         x, y = map(int, input().split())
         cabbage[x][y] = 1
 
     cnt = 0
-    for y in range(N):
-        for x in range(M):
-            if visited[x][y] == 0 and cabbage[x][y] == 1:
+    for x in range(M):
+        for y in range(N):
+            if cabbage[x][y] == 1:
                 dfs(x, y)
                 cnt += 1
-
     print(cnt)
