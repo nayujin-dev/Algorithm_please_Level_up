cb6c946390cc4470d09bbde2fccf8e14e0a7c203
Yujin Na <91943160+nayujin-dev@users.noreply.github.com>
2023-10-17 20:45:40 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -7,17 +7,8 @@ int index[26];
 int main() {
 	std::cin >> S;
 
-	for (int i = 0; i < 26; i++) {
-		index[i] = -1;
-	}
-	for (int i = 0; i < S.size(); i++) {
-		int idx = S[i] - 'a';
-		if (index[idx]<0) 
-			index[idx] = i;
-	}
-
-	for (int i = 0; i <26; i++) {
-		std::cout << index[i] << ' ';
+	for (int i = 'a'; i <= 'z'; i++) {
+		std::cout << (int)S.find(i) << " ";
 	}
 
 	return 0;
