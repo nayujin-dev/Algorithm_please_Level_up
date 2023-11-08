6cdbaa70395e609e3623dafd07c47dbe1d4548df
Yujin Na <91943160+nayujin-dev@users.noreply.github.com>
2023-11-08 13:40:02 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -2,22 +2,22 @@ function solution(jobs) {
     let answer = 0;
     jobs.sort((job1, job2)=>job1[0]-job2[0]);
     
-    let priorityQueue=[];
+    let pq=[];
     let i=0, time=0;
-    while(i< jobs.length || priorityQueue.length!==0){
-        if(i<jobs.length && jobs[i][0]<=time ){
-            priorityQueue.push(jobs[i++]);
-            priorityQueue.sort((a,b) => a[1]-b[1]);
+    while(i< jobs.length || pq.length!==0){
+        if(i<jobs.length && jobs[i][0]<=time){
+            pq.push(jobs[i++]);
+            pq.sort((a,b) => a[1]-b[1]); // 소요시간 오름차순
             continue;
         }
-        if(priorityQueue.length!==0){
-            time+=priorityQueue[0][1]; // minheap 값 가져올것
-            answer+= time - priorityQueue[0][0];
-            priorityQueue.shift(); // minheap pop
-        }else{
+        if(pq.length!==0){
+            time+=pq[0][1]; // minheap 값 가져올것
+            answer+= time - pq[0][0]; // minheap 값 가져올것
+            pq.shift(); // minheap pop
+        }else{ // 우선순위큐는 비었지만, jobs를 다 순회하지 않은 경우 -> 들어온 순서대로
             time = jobs[i][0];
         }
     }
     
-    return Math.floor(answer/jobs.length);
+    return Math.floor(answer/jobs.length); //소수점 이하의 수는 버리기
 }
