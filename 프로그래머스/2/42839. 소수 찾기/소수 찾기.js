d66aa033c34e87985f8e1554e7c79a0d800978b8
Yujin Na <91943160+nayujin-dev@users.noreply.github.com>
2023-11-23 00:03:12 +0900

    [level 2] Title: 소수 찾기, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,10 +1,32 @@
-function solution(phone_book) {
-  phone_book.sort();
-  let answer = true;
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    phone_book[i] === phone_book[i + 1].substr(0, phone_book[i].length) &&
-      (answer = false);
-  }
-
-  return answer;
+function solution(numbers) {
+    var answer = [];
+    let num=numbers.split('');
+    // 가능한 모든 숫자의 순열 찾기
+    const getPermutations=(fixed,arr)=>{
+        // arr: 아직 선택 안된 수 배열
+        // fixed: 선택된수
+        if(arr.length>=1){
+            for(let i=0;i<arr.length;i++){
+                const newNum = fixed+ arr[i];
+                const copyArr=[...arr];
+                copyArr.splice(i,1);
+                if(!answer.includes(+newNum) && isPrimeNum(+newNum)){
+                    answer.push(+newNum);
+                }
+                getPermutations(newNum,copyArr);
+            }
+        }
+    }
+    
+    // 찾은 순열에 대해, 소수인지 확인
+    
+    const isPrimeNum=(n)=>{
+        if(n<=1) return false;
+        for(let i=2;i*i<=n;i++){
+            if(n%i===0) return false;
+        }
+        return true;
+    }
+    getPermutations('',num);
+    return answer.length;
 }
