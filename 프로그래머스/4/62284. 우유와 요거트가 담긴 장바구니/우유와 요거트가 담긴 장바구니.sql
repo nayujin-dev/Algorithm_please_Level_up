c033e2202027cbac698c4dcc60013516149a0d17
Yujin Na <91943160+nayujin-dev@users.noreply.github.com>
2025-03-01 23:41:24 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,11 @@
+-- 코드를 입력하세요
+SELECT distinct cart_id
+from cart_products
+where (cart_id, 'Milk') in (
+    select cart_id, name
+    from cart_products
+) and (cart_id, 'Yogurt') in (
+    select cart_id, name
+    from cart_products
+)
+order by cart_id;
